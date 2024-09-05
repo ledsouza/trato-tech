448990d106925c3de98ac21a0a278cbc792cdfa2
ledsouza <leandro.souza.159@gmail.com>
2024-09-05 15:51:55 -0300

    implementando redux store

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
-import PaginaPadrao from 'components/PaginaPadrao';
-import Home from 'pages/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PaginaPadrao from "components/PaginaPadrao";
+import Home from "pages/Home";
+import Categoria from "pages/Categoria";
 
 export default function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PaginaPadrao />}>
-          <Route index element={<Home/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PaginaPadrao />}>
+                    <Route index element={<Home />} />
+                    <Route
+                        path="/categoria/:nomeCategoria"
+                        element={<Categoria />}
+                    />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
