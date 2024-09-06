c083b7390b7bf89a7ba7fca1e7b14a4a393873b5
ledsouza <leandro.souza.159@gmail.com>
2024-09-06 10:34:29 -0300

    gerenciamento de estados globais com redux

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PaginaPadrao from "components/PaginaPadrao";
-import Home from "pages/Home";
-import Categoria from "pages/Categoria";
+import PaginaPadrao from 'components/PaginaPadrao';
+import Carrinho from 'pages/Carrinho';
+import Categoria from 'pages/Categoria';
+import Home from 'pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PaginaPadrao />}>
-                    <Route index element={<Home />} />
-                    <Route
-                        path="/categoria/:nomeCategoria"
-                        element={<Categoria />}
-                    />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<PaginaPadrao />}>
+          <Route index element={<Home />} />
+          <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
+          <Route path='carrinho' element={<Carrinho />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
